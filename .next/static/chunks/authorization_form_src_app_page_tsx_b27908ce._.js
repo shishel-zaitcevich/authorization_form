1bb91141ac1992a110293531291da06269fe1723
shishel-zaitcevich <ushka_mail.ru>
2025-10-08 02:30:23 +0300

    added animations

--- .next/static/chunks/authorization_form_src_app_page_tsx_b27908ce._.js
+++ .next/static/chunks/authorization_form_src_app_page_tsx_b27908ce._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/54a8a_motion-dom_dist_es_3794f0e8._.js",
-  "static/chunks/54a8a_framer-motion_dist_es_67fc9fa4._.js",
+  "static/chunks/54a8a_framer-motion_dist_es_6329b1f4._.js",
   "static/chunks/54a8a_@mui_material_814a6f48._.js",
   "static/chunks/54a8a_f4844f7f._.js",
   "static/chunks/authorization_form_src_0d9e1fe5._.js"
